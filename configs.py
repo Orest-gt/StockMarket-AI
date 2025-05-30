be2845cb8cf2f5733013bd53cfc2cd6e595a1632
Orest-gt <orestisgatos@gmail.com>
2025-05-30 22:52:09 +0300

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,6 +9,6 @@ main_training_date_end = "2025-05-29"
 add_training_date_start = "2000-01-01"
 add_training_data_end = "2019-12-31"
 predict_data_period = "1d"
-best_model_path = r"C:\Users\Orestis\PycharmProjects\StockMarket-AI\tests\best_model_" + ticker + "-"
+best_model_path = r"PATH-TO-PROJECT-TEST-FILE" + ticker + "-"
 best_model_path_end = ".pth"
 print("  " + best_model_path + "data" + best_model_path_end)
