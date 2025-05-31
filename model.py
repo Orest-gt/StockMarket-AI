17c89dd41d4869ec4de3916fde848b06473ecff5
Orest-gt <orestisgatos@gmail.com>
2025-05-31 13:09:06 +0300

    Erased useless info and added Sequential model

--- model.py
+++ model.py
@@ -1,13 +1,19 @@
 import torch.nn as nn
 
 class Model(nn.Module):
-    def __init__(self, input_size: int=5, hidden_size: int=64, num_layers: int=4):
+    def __init__(self, input_size: int = 5, hidden_size: int= 64, num_layers: int = 3):
         super().__init__()
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers=num_layers, batch_first=True, dropout=0.2)
-        self.mlp = nn.Linear(hidden_size, 1)
+        self.mlp = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size * 4),
+            nn.ReLU(),
+            nn.Linear(hidden_size * 4, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, 1)
+            )
     def forward(self, x):
         output, _ = self.lstm(x)
-        output = output[:, -1, :] # last output (chatgpt)
+        output = output[:, -1, :]
         output = self.mlp(output)
         return output
 
