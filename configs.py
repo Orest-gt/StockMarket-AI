9c6f7ac8205b07571b94702e886fa4d8425ee6ad
Orest-gt <orestisgatos@gmail.com>
2025-06-01 21:00:08 +0300

    Update configs.py

--- configs.py
+++ configs.py
@@ -13,11 +13,3 @@ best_model_path = r"tests" + r"/best_model_" + ticker + "-"
 best_model_path_end = ".pth"
 print("  " + best_model_path + "data" + best_model_path_end)
 epsilon = 1-e4
-
-'''
-2025-05-31-12-47
-2025-05-31-12-48
-2025-05-31-12-49
-2025-05-31-12-50
-2025-05-31-12-51
-'''
