c2748ada47da04f1672770e3cb84b2fb1920635b
Orest-gt <orestisgatos@gmail.com>
2025-06-13 00:47:05 +0300

    Update make_dataset.py

--- make_dataset.py
+++ make_dataset.py
@@ -1,6 +1,6 @@
 import torch
 
-def create_dataset(data: torch.Tensor, window=120): # 60 day dataset
+def create_dataset(data: torch.Tensor, window=120):
     X, y = [], []
     for i in range(window, len(data)):
         X.append(data[i - window:i]) # it was wrong and chatgpt saved it
